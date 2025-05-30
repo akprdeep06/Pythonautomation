c6ed55d59f888a42dab877fa040193a152b41ad2
Pradeep Akshay <pradeepakshay06@gmail.com>
2025-05-30 11:20:49 +0530

    new line code addd

--- even_odd.py
+++ even_odd.py
@@ -2,6 +2,9 @@ n=int(input("Enter the Number")
 for i in range(n):
     if i%2==0:
         print(i,"It is even Number")
+    elif i==0:
+         print("The value is 0") 
     else:
-        print("Odd Number")    
+          print("Odd Number")  
+          
        
